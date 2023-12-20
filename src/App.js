41ebb1a31a3bddc03c2c7cf687296faedb980565
Aboli <aboli@merchantrms.com>
2023-12-20 16:12:49 +0530

    tailwind installation and configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Nomad</h2>
+      <h2 className='text-orange-500 font-bold text-lg'>Nomad</h2>
     </div>
   );
 }
